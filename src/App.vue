f2f65361e1ec606427ec4c15ed6a30de33f32095
sunnyrepo <hahala1993@gmail.com>
2023-12-09 23:09:11 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Button } from '@/components/ui/button'
+import { ref } from 'vue'
+
+
+const open = ref(false)
+
+</script>
+
+<template>
+  <Dialog :open="open">
+    <DialogTrigger>
+      <Button @click="open = true">Edit Profile</Button>
+    </DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Edit profile</DialogTitle>
+        <DialogDescription>
+          Make changes to your profile here. Click save when you're done.
+        </DialogDescription>
+      </DialogHeader>
+
+      <DialogFooter>
+        Save changes
+      </DialogFooter>
+    </DialogContent>
+  </Dialog>
+</template>
